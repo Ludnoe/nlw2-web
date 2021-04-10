d44afb4f2fd75a1eb09a23f84267f8ee7b41dac4
LudNoe <ludnoe@hotmail.com>
2021-04-09 21:26:14 -0300

    Aplicação de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import Landing from './pages/Landing'
+//import Landing from './pages/Landing'
+import Routes from './routes';
 
 import './assets/styles/global.css';
+
 //JSX = JavaScript + XML (HTML dentro do JS)
 
 function App() {
   return (
-   <Landing />
+   <Routes />
   );
 }
 
