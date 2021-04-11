f9cd0d1bd38c4e8d69d1b87984655127b11496ab
LudNoe <ludnoe@hotmail.com>
2021-04-11 18:44:10 -0300

    pagina listagem de professores

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,4 +1,38 @@
 import React from 'react';
 
-import './styles.css'
+import wathsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import './styles.css';
+
+function TeacherItem() {
+  return(
+    <article className="teacher-item">
+        <header>    
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Bertoldo_Klinger_perfil_1930s.jpg" alt="Bertoudo Jesus"/>
+          <div>
+              <strong>Bertoudo Jesus</strong>
+              <span>História</span>
+          </div>
+        </header>
+
+        <p>
+          Ele estudou na University College School e no King's College London antes de entrar no Caius College, Cambridge em 1867. 
+          <br/><br />
+          Migrando para o St John's College, ele ganhou seu B.A. com uma primeira classe no Natural Sciences Tripos e obteve uma bolsa universitária – a primeira em seu assunto – em 1873.  
+        </p>
+
+        <footer>
+          <p>
+            Preço/hora
+            <strong>R$ 5,00</strong>  
+          </p>
+          <button type="button">
+            <img src={wathsappIcon} alt="Whatsapp"/>
+            Entrar em contato
+          </button>
+        </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
-import wathsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,32 +29,10 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>    
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Bertoldo_Klinger_perfil_1930s.jpg" alt="Bertoudo Jesus"/>
-                        <div>
-                            <strong>Bertoudo Jesus</strong>
-                            <span>História</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Ele estudou na University College School e no King's College London antes de entrar no Caius College, Cambridge em 1867. 
-                        <br/><br />
-                        Migrando para o St John's College, ele ganhou seu B.A. com uma primeira classe no Natural Sciences Tripos e obteve uma bolsa universitária – a primeira em seu assunto – em 1873.  
-                    </p>
-
-                    <footer>
-                        <p>
-                          Preço/hora
-                          <strong>R$ 5,00</strong>  
-                        </p>
-                        <button type="button">
-                            <img src={wathsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article> 
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
             </main>
         </div>
     )
