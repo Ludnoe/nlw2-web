ce8967e8dbff5708fdceb7167e2eb55bc1e44519
LudNoe <ludnoe@hotmail.com>
2021-04-11 17:07:04 -0300

    estilização TeacherList

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
-    return(
-        <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas." />
-        </div>
-    )
+  return(
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incrível que você quer dar aulas." />
+    </div>
+  )
 }
 
 export default TeacherForm; 
